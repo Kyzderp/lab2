10b94af0abd76de97954e356bfb481f8ddf4eb96
Kyzderp <hannah_chu@yahoo.com>
2016-10-20 12:47:16 -0700

    my stuff

---
+++ static/js/portfolio.js
@@ -0,0 +1,23 @@
+function troll()
+{
+	alert("TROLOLOLOL THAT BUTTON DOESN'T WORK.\nReally though. JavaScript and emails don't go together.\nJust shoot an email to hannah_chu@yahoo.com\nOr spam the Facebook icon below.");
+}
+
+function main()
+{
+}
+
+/*
+For the works, caption + hover would be good. Like "what am I clicking on?"
+Add info about the skill set that I applied for each work
+Expand more on the about, another paragraph of that size would be good, it needs to be more personal and make the user feel like they know me as a person after reading it.
+Add resume, probably in one of the top tabs.
+When clicking on contact, it's not clear because the icons are in the footer, can add a section for form submission.
+Portfolio is good in that it's concrete, "this is what I've done"
+Maybe right under the large name at the top, then immediately the position (undergraduate cs major at UCSD), then read the more refined bio
+Add caption over stuff / watermark, thought at first that I didn't create those images and that I'd just found them on the web.
+Works should open in new window so that the user still stays on the main page.
+Should have a pic of self, but doesn't have to be a real picture, could be some sort of representation like https://github.com/AlexOuyang
+Make the banner with real name instead of Kyzer, because people don't know who Kyzer is
+Have a new page with description for each work.
+*/
